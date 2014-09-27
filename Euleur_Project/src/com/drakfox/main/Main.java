48b54b15765d061692cd8e065c267664cc95033a
drakfox <maxime.rodier.95@gmail.com>
2014-09-27 12:55:59 -0400

    Initial Commit

---
+++ Euleur_Project/src/com/drakfox/main/Main.java
@@ -0,0 +1,12 @@
+package com.drakfox.main;
+
+import com.drakfox.problème___2.Problème___2;
+
+public class Main {
+	
+	public static void main(String[] arg0)
+	{
+		new Problème___2();
+	}
+
+}
---
+++ Euleur_Project/src/com/drakfox/problème___2/Problème___2.java
@@ -0,0 +1,41 @@
+package com.drakfox.problème___2;
+
+import java.util.ArrayList;
+
+public class Problème___2 {
+	public static final int SequenceMaxValue = 4000000;
+	public int Sum = 0;
+	public ArrayList<Integer> FibonacciSequence = new ArrayList<Integer>();
+	
+	
+	public Problème___2()
+	{
+		FibonacciSequence.add(1);
+		FibonacciSequence.add(2);
+		
+		for(int i = 0; FibonacciSequence.get(i) < SequenceMaxValue; i++)
+		{
+			if(FibonacciSequence.get(i) > SequenceMaxValue)
+			{
+				return;
+			}
+			
+			FibonacciSequence.add(FibonacciSequence.get(i)+FibonacciSequence.get(i+1));
+		}
+		
+		for(int i = 0; i < FibonacciSequence.size(); i++)
+		{
+			int Number = FibonacciSequence.get(i);
+			
+			float Nombre = ((float)FibonacciSequence.get(i))/2f;
+			
+			
+			if(Float.compare(Nombre, (float)Math.floor((double)Nombre)) == 0)
+			{
+				Sum += Number;
+			}
+		}
+		
+		System.out.print(Sum);
+	}
+}
