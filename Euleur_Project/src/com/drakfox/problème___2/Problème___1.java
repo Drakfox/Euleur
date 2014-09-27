f7c7299e6fed63b8f7ff8e1576620e312d935959
drakfox <maxime.rodier.95@gmail.com>
2014-09-27 13:02:28 -0400

    Problème___1

---
+++ Euleur_Project/src/com/drakfox/problème___2/Problème___1.java
@@ -0,0 +1,28 @@
+package com.drakfox.problème___2;
+
+public class Problème___1 {
+	
+	public static final int Size = 1000;
+	public int Sum = 0;
+	
+	public Problème___1()
+	{
+		for(int i = 0; i < Size; i++)
+		{
+			float Nombre3 = ((float)i)/3f;
+			float Nombre5 = ((float)i)/5f;
+				
+			if(Float.compare(Nombre3, (float)Math.floor((double)Nombre3)) == 0)
+			{
+				Sum += i;
+			}
+			else if(Float.compare(Nombre5, (float)Math.floor((double)Nombre5)) == 0)
+			{
+				Sum += i;
+			}
+			
+		}
+		
+		System.out.print(Sum);
+	}
+}
